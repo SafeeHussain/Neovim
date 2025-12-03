dd190cb3aad1f2a78a9210aa23309f18e4366d5c
SafeeHussain <161167937+SafeeHussain@users.noreply.github.com>
2025-12-03 20:27:37 +0000

    Updated changes
    Saw some issues that there are clashes with old and new files, this has been fully reuploaded to now be working

---
+++ lua/options/keymaps.lua
@@ -0,0 +1,59 @@
+-- leader key
+vim.g.mapleader = " "
+
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- MOVEMENT KEYBINDS:
+
+-- Unmapping all arrow keys while in normal mode
+--vim.cmd("nnoremap <Left> :echo 'h to go left'<CR>")
+--vim.cmd("nnoremap <Right> :echo 'l to go right'<CR>")
+--vim.cmd("nnoremap <Up> :echo 'k to go up'<CR>")
+--vim.cmd("nnoremap <Down> :echo 'j to go down'<CR>")
+-- Unmapping all arrow keys while in insert mode
+-- vim.cmd("inoremap <Left> <Nop>")
+-- vim.cmd("inoremap <Right> <Nop>")
+-- vim.cmd("inoremap <Up> <Nop>")
+-- vim.cmd("inoremap <Down> <Nop>")
+-- Converts Alt + hjkl to be the movement keys whilst in insert mode
+keymap("i", "<A-k>", "<Up>", opts)
+keymap("i", "<A-j>", "<Down>", opts)
+keymap("i", "<A-h>", "<Left>", opts)
+keymap("i", "<A-l>", "<Right>", opts)
+
+-- Redirects the directory to .config
+vim.keymap.set('n', '<leader>cd', function()
+  vim.cmd('cd ~/.config/nvim')
+end, { desc = 'nvim config dir' })
+
+-- Reenables the ctrl+backspace as the word delete in insert mode (as it should)
+vim.keymap.set('i', '<C-H>', '<C-W>', { desc = 'Delete previous word with Ctrl+Backspace' })
+
+
+-- PLUGIN KEYBINDS:
+
+-- MdMath Keybind
+vim.keymap.set("n", "<C-.>", ":lua ToggleMdMath()<CR>", { noremap = true, silent = true })
+-- Neo-tree Keybind
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
+-- Toggle-term opening terminal session
+-- (Rest of terms can be found in the toggleterm.lua file)
+vim.api.nvim_set_keymap(
+			"n",
+			"<leader>th",
+			":Neotree filesystem reveal left toggle<CR> :ToggleTerm<CR> <Cmd>wincmd k<CR> :Neotree filesystem reveal left toggle<CR> <C-w>l<CR> <C-w>j",
+			{ noremap = true, silent = true }
+		)
+-- stay-centered Keybind
+vim.keymap.set({ 'n', 'v' }, '<leader>st', function() require('stay-centered').toggle() end, { desc = 'Toggle stay-centered.nvim' })
+
+-- lua/options/keymaps.lua
+
+-- Other important keymap notes
+-- Ctrl + f for full page down
+-- Ctrl + b for full page up
+-- Ctrl + d for half page down
+-- Ctrl + u for half page up
+-- $ for end of a line
+-- ^ for first non-blank character of a line
---
+++ lua/plugins/coding/neotest.lua
@@ -0,0 +1,10 @@
+-- library for asyncrhonous IO in neovim
+-- provides a simple interface to lua coroutines
+-- simplifies the writing of async plugins and code for neovim
+-- provides APIs (Application Programming Interface, set of rules and protocols that allows different software applications to communicate with each other)
+-- Neotest provides APIs to control asyncrhonous flows
+-- Neotest provides commands and panels to run, stop and debug tests from inside neovim)
+
+return {
+  "nvim-neotest/nvim-nio",
+}
